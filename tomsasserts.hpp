4020227b108389ed06ca0323f5b56b3281f8e3e7
Tom Burnip <tb@tburnip.co.uk>
2021-02-20 19:54:18 +0000

    More readable printout on bytes equal

--- tomsasserts.hpp
+++ tomsasserts.hpp
@@ -24,27 +24,13 @@ void print_buf(void* buf,int len,int max) {
     putchar('\n');
 }
 
-#define ASSERT(expression,FailReason) if ((expression)==false) { \
-    printf("Test:\t\t%s\nFile:\t\t%s\nLine Number:\t%d\nExpression:\t%s\n",FailReason,__FILE__,__LINE__,#expression); \
-    return -1; \
-}
 
-#define ASSERT_ARRAYS_EQUAL(arr1,arr2,len,FailReason) for RANGE(i,len) { \
-    ASSERT(arr1[i]==arr2[i],FailReason); \
-}
+#define ASSERT(expression,FailReason) if ((expression)==false) { printf("Test:\t\t%s\nFile:\t\t%s\nLine Number:\t%d\nExpression:\t%s\n",FailReason,__FILE__,__LINE__,#expression); return -1;}
 
-#define ASSERT_BYTES_EQUAL(buf1,buf2,len,FailReason) if (memcmp(buf1,buf2,len)!=0) { \
-    printf("Test:\t\t%s\nFile:\t\t%s\nLine Number:\t%d\nBytesneq:\t%s\t%s\n",FailReason,__FILE__,__LINE__,#buf1,#buf2); \
-    print_buf(buf1,len,16); \
-    print_buf(buf2,len,16); \
-    return -1; \
-}
+#define ASSERT_ARRAYS_EQUAL(arr1,arr2,len,FailReason) for RANGE(i,len) { ASSERT(arr1[i]==arr2[i],FailReason);}
 
-#define ASSERT_ARRAY_ZERO_FROM(arr,from,to,FailReason) for RANGE_FROM_TO(i,from,to) { \
-    ASSERT(arr[i]==0,FailReason); \
-} // from and to are inclusive.
+#define ASSERT_BYTES_EQUAL(buf1,buf2,len,FailReason) if (memcmp(buf1,buf2,len)!=0) { printf("Test:\t\t%s\nFile:\t\t%s\nLine Number:\t%d\nBytesneq:\t%s\t%s\n",FailReason,__FILE__,__LINE__,#buf1,#buf2); print_buf(buf1,len,16); print_buf(buf2,len,16); return -1;}
 
-#define ASSERT_ERROR(expression) if ((expression)!=true) { \
-    printf("File:\t\t%s\nLine Number:\t%d\nExpression:\t%s\n",__FILE__,__LINE__,#expression); \
-    return -1; \
-}
+#define ASSERT_ARRAY_ZERO_FROM(arr,from,to,FailReason) for RANGE_FROM_TO(i,from,to) { ASSERT(arr[i]==0,FailReason);} // from and to are inclusive.
+
+#define ASSERT_ERROR(expression) if ((expression)!=true) { printf("File:\t\t%s\nLine Number:\t%d\nExpression:\t%s\n",__FILE__,__LINE__,#expression); return -1;}
